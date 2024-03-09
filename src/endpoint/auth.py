4a59853e84dd4dc9063ce75ef6a106d9981365c3
ssabrut <michael.gunawan2002@gmail.com>
2024-03-09 23:55:03 +0700

    Add login endpoint and refine register endpoint

--- src/endpoint/auth.py
+++ src/endpoint/auth.py
@@ -44,6 +44,47 @@ async def register(request: Request):
             "timestamp": time.time()
         })
         
-        return Response(status=201, message="success", token=token, value=data)
+        return Response(status=201, message="success", token=str(token), value=str(user.inserted_id))
+    except Exception as e:
+        return Response(status=500, message="error", value=str(e))
+
+@router.post('/login', response_model=Response)
+async def login(request: Request):
+    """
+    Login a user.
+
+    Args:
+        request (Request): The request object.
+
+    Returns:
+        Response: The response object.
+    """
+    
+    data = await request.json()
+
+    if 'username' not in data or 'password' not in data:
+        return Response(status=409, message="error", value="Username and password are required!")
+
+    try:
+        data = {
+            "username": data['username'],
+            "password": hashlib.sha256(data['password'].encode()).hexdigest(),
+        }
+        
+        user = await user_collection.find_one({"username": data['username']})
+        if not user:
+            return Response(status=404, message="error", value="User not found!")
+
+        if user['password'] != data['password']:
+            return Response(status=401, message="error", value="Invalid password!")
+
+        token = jwt.encode({'_id': str(user['_id'])}, "SECRET", algorithm="HS256")
+        await token_collection.insert_one({
+            "user_id": str(user['_id']),
+            "token": token,
+            "timestamp": time.time()
+        })
+
+        return Response(status=200, message="success", token=str(token), value=str(user['_id']))
     except Exception as e:
         return Response(status=500, message="error", value=str(e))
