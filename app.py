41903b478586ae0be10474e014f8872d6f8ba671
ssabrut <michael.gunawan2002@gmail.com>
2024-03-09 11:37:32 +0700

    Refine description prompt for output

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ import os
 
 load_dotenv()
 
-with open("src/prompts/title.txt", "r") as file:
+with open("src/prompts/description.txt", "r") as file:
     prompt = file.read()
-#     name = "E-Commerce store for keyboard enthusiast"
+    name = "E-Commerce store for keyboard enthusiast"
 
-#     goals = """A platform for keyboard enthusiasts to buy and sell custom keyboards, keycaps, and other accessories. Also have a community forum for users to discuss and share their builds."""
+    goals = """A platform for keyboard enthusiasts to buy and sell custom keyboards, keycaps, and other accessories. Also have a community forum for users to discuss and share their builds."""
 
-#     technologies = """1. MongoDB
-# 2. Express.js
-# 3. React.js
-# 4. Node.js"""
+    technologies = """1. MongoDB
+2. Express.js
+3. React.js
+4. Node.js"""
 
-#     # replace [NAME] on prompt
-#     prompt = prompt.replace("[NAME]", name)
+    # replace [NAME] on prompt
+    prompt = prompt.replace("[NAME]", name)
 
-#     prompt = prompt.replace("[GOALS]", goals)
-#     prompt = prompt.replace("[TECHNOLOGIES]", technologies)
+    prompt = prompt.replace("[GOALS]", goals)
+    prompt = prompt.replace("[TECHNOLOGIES]", technologies)
 
 claude = Claude(api_key=os.getenv("ANTHROPIC_API_KEY"))
 response = claude.generate("I want to create E-Commerce store for keyboard enthusiast", prompt)
