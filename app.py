6ed675ed4b4ac3ac28285e5ebdf368e214c14a8a
ssabrut <michael.gunawan2002@gmail.com>
2024-03-09 12:41:03 +0700

    Add installation prompt

--- app.py
+++ app.py
@@ -4,21 +4,22 @@ import os
 
 load_dotenv()
 
-with open("src/prompts/description.txt", "r") as file:
+with open("src/prompts/installation.txt", "r") as file:
     prompt = file.read()
-    name = "E-Commerce store for keyboard enthusiast"
+    # name = "E-Commerce store for keyboard enthusiast"
 
-    goals = """A platform for keyboard enthusiasts to buy and sell custom keyboards, keycaps, and other accessories. Also have a community forum for users to discuss and share their builds."""
+    # goals = """A platform for keyboard enthusiasts to buy and sell custom keyboards, keycaps, and other accessories. Also have a community forum for users to discuss and share their builds."""
 
+    url = "https://github.com/ssabrut/readme-writer.git"
     technologies = """1. MongoDB
 2. Express.js
 3. React.js
 4. Node.js"""
 
     # replace [NAME] on prompt
-    prompt = prompt.replace("[NAME]", name)
+    prompt = prompt.replace("[URL]", url)
 
-    prompt = prompt.replace("[GOALS]", goals)
+    # prompt = prompt.replace("[GOALS]", goals)
     prompt = prompt.replace("[TECHNOLOGIES]", technologies)
 
 claude = Claude(api_key=os.getenv("ANTHROPIC_API_KEY"))
